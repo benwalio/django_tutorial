0d826be14e9e01d09cd261937e0fb5dafcf3f32c
Ben Wallace <ben@Bens-MacBook-Pro.local>
2017-11-05 21:05:27 -0500

    worked on views

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Book, Author, BookInstance, Genre
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page
+    """
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact = "a").count()
+    num_authors = Author.objects.count()
+
+    return render(
+        request,
+        "index.html",
+        context = {"num_books": num_books, "num_instances": num_instances, "num_instances_available": num_instances_available, "num_authors": num_authors},
+    )
